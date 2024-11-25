2b3ddc56e1764a041a6fe6631eedc69316a0a039
27355014 <27355014@L815WD220003.edu.cbe.ab.ca>
2024-11-25 10:37:18 -0700

    1

---
+++ Chapter7/src/Skillbuilders/Rectangle.java
@@ -0,0 +1,95 @@
+package Skillbuilders;
+
+public class Rectangle 
+{
+	private double length, width;
+	
+	public Rectangle() //Default Constructor method
+	{
+		length = 1;
+		width = 1;		
+	}
+	
+	//Overloading the default constructor method
+	public Rectangle(double l, double w)
+	{
+		length = l;
+		width = w;
+	}
+	
+	//Modifier methods start with set
+	public void setLength(double newL)
+	{
+		length = newL;
+	}
+	
+	public void setWidth(double newW)
+	{
+		width = newW;
+	}
+	
+	//Accessor methods start with get
+	public double getLength()
+	{
+		return length;
+		
+	}
+	
+	public double getWidth()
+	{
+		return width;
+		
+	}
+	
+	public String toString() 
+	{
+		String rectangleString;
+
+		rectangleString = "Rectangle has length " + length + " and width " + width;
+	 	return(rectangleString);
+	}
+	
+	
+	public double area()
+	{
+		double rectArea;
+		
+		rectArea = length * width;
+		
+		return rectArea;
+	}
+	
+	public boolean equals(Object r)
+	{
+		Rectangle testObj = (Rectangle)r;
+		
+		if(testObj.getLength() == length && testObj.getWidth() == width)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+		
+	}
+
+	public int compareTo(Object r)
+	{
+		Rectangle testObj = (Rectangle)r;
+		
+		if(length < testObj.getLength() || width < testObj.getWidth())
+		{
+			return(-1);
+		}
+		else if(length == testObj.getLength() || width == testObj.getWidth())
+		{
+			return(0);
+		}
+		else
+		{
+			return(1);
+		}
+	}
+
+}
