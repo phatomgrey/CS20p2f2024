b3775dbcf053a9140316ef3d74ffa46f3a535fc0
27355014 <27355014@L815WD220003.edu.cbe.ab.ca>
2024-12-12 09:22:36 -0700

    1

--- GettingStarted/src/ReadButton.java
+++ GettingStarted/src/ReadButton.java
@@ -2,7 +2,7 @@
 //Add Phidgets Library | You added a file called phidget22 when configuring your project. Import gives you access to the Phidgets library code inside that file. 
 import com.phidget22.*;
 
-public class GettingStarted {
+public class ReadButton {
     //Handle Exceptions | Exceptions will happen in your code from time to time. These are caused by unexpected things happening. Make sure you’ve added "throws Exception" to your main method.
     public static void main(String[] args) throws Exception{
         
@@ -14,7 +14,7 @@ public class GettingStarted {
         redButton.setIsHubPortDevice(true);
 
         //Open | Open establishes a connection between your object and your physical Phidget. You provide a timeout value of 1000 to give the program 1000 milliseconds (1 second) to locate your Phidget. If your Phidget can't be found, an exception will be thrown.
-        redButton.open(10);
+        redButton.open(1000);
 
         //Use your Phidgets | Here is where you use your Phidgets! This code checks the state of the button and prints true/false when the button is pressed/released. The sleep function means the button state is only checked every 150 milliseconds. Sleeping is used to make it easier to read the console output and to put less stress on your CPU.
         while(true){
