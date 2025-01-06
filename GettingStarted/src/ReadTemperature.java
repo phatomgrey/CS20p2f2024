7144c0e13349b797f62aeb100ced90b5389bef66
silla <silla@10.0.0.52>
2025-01-05 17:31:18 -0700

    1

--- GettingStarted/src/ReadTemperature.java
+++ GettingStarted/src/ReadTemperature.java
@@ -1,19 +1,21 @@
-
-//Add Phidgets Library | You added a file called phidget22 when configuring your project. Import gives you access to the Phidgets library code inside that file. 
+// Add Phidgets Library | You added a file called phidget22 when configuring your project. Import gives you access to the Phidgets library code inside that file.
 import com.phidget22.*;
 
 public class ReadTemperature {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
-        //Create | Here you have created a TemperatureSensor object. TemperatureSensor is a class in your Phidgets library that gathers temperature data from your Phidget. 
+        // Create | Here you have created a TemperatureSensor object. TemperatureSensor is a class in your Phidgets library that gathers temperature data from your Phidget. 
         TemperatureSensor temperatureSensor = new TemperatureSensor();
 
-        //Open | Open establishes a connection between your object and your physical Phidget. You provide a timeout value of 1000  to give the program 1000 milliseconds (1 second) to locate your Phidget. If your Phidget can't be found, an exception will be thrown.
+        // Open | Open establishes a connection between your object and your physical Phidget. You provide a timeout value of 1000 to give the program 1000 milliseconds (1 second) to locate your Phidget. If your Phidget can't be found, an exception will be thrown.
         temperatureSensor.open(1000);
 
-        //Use your Phidgets | This code will print the temperature every 150ms
+        // Use your Phidgets | This code will print the temperature in both Celsius and Fahrenheit every 150ms
         while (true) {
-            System.out.println("Temperature: " + temperatureSensor.getTemperature() + " °C" );
+            double celsius = temperatureSensor.getTemperature();
+            double fahrenheit = (celsius * 1.8) + 32; // Convert Celsius to Fahrenheit
+            
+            System.out.println("Temperature: " + celsius + " °C / " + fahrenheit + " °F");
             Thread.sleep(150);
         }
     }
