2d959bc07f32b1bfca2678ec8dfba927fdca3888
27355014 <27355014@L815WD220003.edu.cbe.ab.ca>
2025-01-07 09:07:07 -0700

    1

--- GettingStarted/src/BlinkLED.java
+++ GettingStarted/src/BlinkLED.java
@@ -1,4 +1,3 @@
-
 // Add Phidgets Library | You added a file called phidget22 when configuring your project. Import gives you access to the Phidgets library code inside that file.
 import com.phidget22.*;
 public class BlinkLED {
